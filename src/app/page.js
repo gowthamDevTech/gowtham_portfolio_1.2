31841bc09f0fb82678dc9a81a10a9b18ea36e361
rumi-dev <gowthamraajmg@gmail.com>
2025-03-19 17:58:23 +0530

    experience issue checking

--- src/app/page.js
+++ src/app/page.js
@@ -4,7 +4,7 @@ import AboutSection from "./components/homepage/about";
 // // import Blog from "./components/homepage/blog";
 import ContactSection from "./components/homepage/contact";
 import Education from "./components/homepage/education";
-// import Experience from "./components/homepage/experience";
+import Experience from "./components/homepage/experience";
 import HeroSection from "./components/homepage/hero-section";
 import Projects from "./components/homepage/projects";
 import Skills from "./components/homepage/skills";
@@ -32,7 +32,7 @@ export default function Home() {
       <h1>testingg</h1>
       <HeroSection />
       <AboutSection />
-      {/* <Experience /> */}
+      <Experience />
       <Skills />
       <Projects />
       <Education />
