bcfc5f6a072c8999b0e342a0e2ee3e7489832836
rumi-dev <gowthamraajmg@gmail.com>
2025-03-19 17:47:34 +0530

    skills section checking

--- src/app/page.js
+++ src/app/page.js
@@ -4,10 +4,10 @@ import AboutSection from "./components/homepage/about";
 // // import Blog from "./components/homepage/blog";
 import ContactSection from "./components/homepage/contact";
 // import Education from "./components/homepage/education";
-import Experience from "./components/homepage/experience";
+// import Experience from "./components/homepage/experience";
 import HeroSection from "./components/homepage/hero-section";
 import Projects from "./components/homepage/projects";
-// import Skills from "./components/homepage/skills";
+import Skills from "./components/homepage/skills";
 
 // async function getData() {
 //   const res = await fetch(`https://dev.to/api/articles?username=${personalData.devUsername}`)
@@ -32,8 +32,8 @@ export default function Home() {
       <h1>testingg</h1>
       <HeroSection />
       <AboutSection />
-      <Experience />
-      {/* <Skills /> */}
+      {/* <Experience /> */}
+      <Skills />
       <Projects />
       {/* <Education /> */}
       {/* <Blog blogs={blogs} /> */}
