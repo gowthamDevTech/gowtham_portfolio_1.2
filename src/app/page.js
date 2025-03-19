6ce5baf9e733ae8c6c63cb943c8ae2324da33116
rumi-dev <gowthamraajmg@gmail.com>
2025-03-19 17:43:20 +0530

    about section checking

--- src/app/page.js
+++ src/app/page.js
@@ -1,6 +1,6 @@
 "use client";
 // import { personalData } from "@/utils/data/personal-data";
-// import AboutSection from "./components/homepage/about";
+import AboutSection from "./components/homepage/about";
 // // import Blog from "./components/homepage/blog";
 import ContactSection from "./components/homepage/contact";
 // import Education from "./components/homepage/education";
@@ -31,7 +31,7 @@ export default function Home() {
     <div>
       <h1>testingg</h1>
       <HeroSection />
-      {/* <AboutSection /> */}
+      <AboutSection />
       {/* <Experience /> */}
       {/* <Skills /> */}
       <Projects />
