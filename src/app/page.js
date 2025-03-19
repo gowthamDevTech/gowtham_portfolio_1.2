edb1ac535cb45614b80d7169c49b8d0ca183ce4d
rumi-dev <gowthamraajmg@gmail.com>
2025-03-19 14:37:26 +0530

    changes

--- src/app/page.js
+++ src/app/page.js
@@ -1,13 +1,13 @@
 "use client";
 // import { personalData } from "@/utils/data/personal-data";
-import AboutSection from "./components/homepage/about";
-// import Blog from "./components/homepage/blog";
-import ContactSection from "./components/homepage/contact";
-import Education from "./components/homepage/education";
-import Experience from "./components/homepage/experience";
-import HeroSection from "./components/homepage/hero-section";
-import Projects from "./components/homepage/projects";
-import Skills from "./components/homepage/skills";
+// import AboutSection from "./components/homepage/about";
+// // import Blog from "./components/homepage/blog";
+// import ContactSection from "./components/homepage/contact";
+// import Education from "./components/homepage/education";
+// import Experience from "./components/homepage/experience";
+// import HeroSection from "./components/homepage/hero-section";
+// import Projects from "./components/homepage/projects";
+// import Skills from "./components/homepage/skills";
 
 // async function getData() {
 //   const res = await fetch(`https://dev.to/api/articles?username=${personalData.devUsername}`)
