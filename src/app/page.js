268dc78109953bdde60d003629103490c910cf5a
rumi-dev <gowthamraajmg@gmail.com>
2025-03-19 14:17:28 +0530

    removed_scrollToTop_for_deploy

--- src/app/page.js
+++ src/app/page.js
@@ -1,3 +1,4 @@
+"use client";
 // import { personalData } from "@/utils/data/personal-data";
 import AboutSection from "./components/homepage/about";
 // import Blog from "./components/homepage/blog";
@@ -26,7 +27,8 @@ export default function Home() {
   // const blogs = await getData();
 
   return (
-    <div suppressHydrationWarning>
+    // <div suppressHydrationWarning>
+    <div>
       <HeroSection />
       <AboutSection />
       <Experience />
