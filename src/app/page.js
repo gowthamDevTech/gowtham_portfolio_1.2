c3055f68c1916ad44057a8daa5542a2479993637
rumi-dev <gowthamraajmg@gmail.com>
2025-03-19 17:38:49 +0530

    about section checking

--- src/app/page.js
+++ src/app/page.js
@@ -2,7 +2,7 @@
 // import { personalData } from "@/utils/data/personal-data";
 // import AboutSection from "./components/homepage/about";
 // // import Blog from "./components/homepage/blog";
-// import ContactSection from "./components/homepage/contact";
+import ContactSection from "./components/homepage/contact";
 // import Education from "./components/homepage/education";
 // import Experience from "./components/homepage/experience";
 import HeroSection from "./components/homepage/hero-section";
@@ -37,7 +37,7 @@ export default function Home() {
       {/* <Projects /> */}
       {/* <Education /> */}
       {/* <Blog blogs={blogs} /> */}
-      {/* <ContactSection /> */}
+      <ContactSection />
     </div>
   );
 }
