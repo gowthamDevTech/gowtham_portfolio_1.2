83a2618749b3d732647a1326f411b1b8244ceb32
rumi-dev <gowthamraajmg@gmail.com>
2025-03-19 17:49:47 +0530

    education section checking

--- src/app/page.js
+++ src/app/page.js
@@ -3,7 +3,7 @@
 import AboutSection from "./components/homepage/about";
 // // import Blog from "./components/homepage/blog";
 import ContactSection from "./components/homepage/contact";
-// import Education from "./components/homepage/education";
+import Education from "./components/homepage/education";
 // import Experience from "./components/homepage/experience";
 import HeroSection from "./components/homepage/hero-section";
 import Projects from "./components/homepage/projects";
@@ -35,7 +35,7 @@ export default function Home() {
       {/* <Experience /> */}
       <Skills />
       <Projects />
-      {/* <Education /> */}
+      <Education />
       {/* <Blog blogs={blogs} /> */}
       <ContactSection />
     </div>
