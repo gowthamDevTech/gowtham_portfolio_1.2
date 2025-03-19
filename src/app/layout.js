c83b531a18035e0be39e05354b9ee0f84e45f868
rumi-dev <gowthamraajmg@gmail.com>
2025-03-19 16:30:55 +0530

    tailwind deploy checking

--- src/app/layout.js
+++ src/app/layout.js
@@ -7,6 +7,7 @@ import "react-toastify/dist/ReactToastify.css";
 // import Navbar from "./components/navbar";
 import "./css/card.scss";
 import "./css/globals.scss";
+import "./globals.css";
 const inter = Inter({ subsets: ["latin"] });
 
 // export const metadata = {
