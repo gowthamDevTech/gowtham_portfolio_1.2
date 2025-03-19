c5f5a5e11d1d39dbdcf43848d49472bca0aa3c96
rumi-dev <gowthamraajmg@gmail.com>
2025-03-19 14:24:37 +0530

    removed navbar footerfor deploy test

--- src/app/layout.js
+++ src/app/layout.js
@@ -22,11 +22,11 @@ export default function RootLayout({ children }) {
       <body className={inter.className}>
         {/* <ToastContainer /> */}
         <main className="min-h-screen relative mx-auto px-6 sm:px-12 lg:max-w-[70rem] xl:max-w-[76rem] 2xl:max-w-[92rem] text-white">
-          <Navbar />
+          {/* <Navbar /> */}
           {children}
           {/* <ScrollToTop /> */}
         </main>
-        <Footer />
+        {/* <Footer /> */}
       </body>
       {/* <GoogleTagManager gtmId={process.env.NEXT_PUBLIC_GTM} /> */}
     </html>
