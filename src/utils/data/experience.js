ddea7a31e57cf21bd0a91ef3f936a50197fee40d
rumi-dev <gowthamraajmg@gmail.com>
2025-03-19 13:43:32 +0530

    first commit

---
+++ src/utils/data/experience.js
@@ -0,0 +1,32 @@
+export const experiences = [
+  {
+    id: 1,
+    title: "Student-Centric E-Learning Platform for Grades 1–12",
+    company:
+      "Woza Geleza is an educational platform designed to provide comprehensive learning resources for students from Grade 1 to Grade 12. ",
+    // company: "Teton Private Ltd.",
+    // duration: "(Jan 2022 - Present)"
+    duration: "React Application",
+  },
+
+  {
+    id: 2,
+    title:
+      "Digital Transformation of Safety Department Reporting in Manufacturing Industry",
+    company:
+      "Application for paper forms digital digital forms for safety inspections, incident reports, and compliance checks.",
+    duration: "Flutter Application",
+  },
+  // {
+  //   id: 2,
+  //   title: "FullStack Developer",
+  //   company: "Fiverr (freelance)",
+  //   duration: "(Jun 2021 - Jan 2022)",
+  // },
+  // {
+  //   id: 3,
+  //   title: "Self Employed",
+  //   company: "Code and build something in everyday.",
+  //   duration: "(Jan 2018 - Present)",
+  // },
+];
---
+++ src/utils/data/personal-data.js
@@ -0,0 +1,19 @@
+export const personalData = {
+  name: "Gowtham",
+  profile: "/profile.png",
+  designation: "Web & Mobile Application Developer",
+
+  description:
+    " My name is Gowtham.I'm a Web & Mobile Application Developer who creates seamless digital experiences using Next.js for web and Flutter for mobile applications. I am available for any kind of job opportunity that suits my skills and interests.",
+  email: "gowtham.dev.mail@gmail.com",
+  phone: "+91 6374099576",
+  address: "Annur,Coimbatore,Tamilnadu - 641653 ",
+  github: "https://github.com/gowthamDevTech",
+  facebook: "https://www.instagram.com/gowtham_athithya/",
+  linkedIn: "https://www.linkedin.com/in/gowtham-mg-2b025317b/",
+  twitter: "https://x.com/ga01012000",
+  stackOverflow: "https://stackoverflow.com/users/29960407/gowtham-athithya",
+  leetcode: "https://leetcode.com/u/gowthamraajmg/",
+  devUsername: "",
+  resume: "",
+};
