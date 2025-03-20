3f8011f5d006bccfd1239a479980d13d9e2309d4
rumi-dev <gowthamraajmg@gmail.com>
2025-03-20 18:47:23 +0530

    navbar footer uncommented

--- src/app/layout.js
+++ src/app/layout.js
@@ -2,9 +2,9 @@
 import { Inter } from "next/font/google";
 // import { ToastContainer } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
-// import Footer from "./components/footer";
+import Footer from "./components/footer";
 // import ScrollToTop from "./components/helper/scroll-to-top";
-// import Navbar from "./components/navbar";
+import Navbar from "./components/navbar";
 import "./css/card.scss";
 import "./css/globals.scss";
 import "./globals.css";
@@ -23,11 +23,11 @@ export default function RootLayout({ children }) {
       <body className={inter.className}>
         {/* <ToastContainer /> */}
         <main className="min-h-screen relative mx-auto px-6 sm:px-12 lg:max-w-[70rem] xl:max-w-[76rem] 2xl:max-w-[92rem] text-white">
-          {/* <Navbar /> */}
+          <Navbar />
           {children}
           {/* <ScrollToTop /> */}
         </main>
-        {/* <Footer /> */}
+        <Footer />
       </body>
       {/* <GoogleTagManager gtmId={process.env.NEXT_PUBLIC_GTM} /> */}
     </html>
